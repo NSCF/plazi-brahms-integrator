010a60bc4a8b64292793aa41b0b1d1738193b7af
Ian Engelbrecht <ianicus.za@gmail.com>
2024-08-15 20:17:31 +0200

    first attempt to fetch an article

--- scripty.py
+++ scripty.py
@@ -1,24 +1,26 @@
 ## remember env\Scripts\activate.bat before starting
 
 import requests
+from urllib.parse import quote
 
+# for later
 base = 'https://api.plazi.org/v1'
 format = '&format=json'
 
-# just for testing out requests, will remove shortly
-PIXABAY_API_KEY = "11111111-7777777777777777777777777"
-base_url = "https://pixabay.com/api/"
-base_params = {
-    "key": PIXABAY_API_KEY,
-    "q": "fruit",
-    "image_type": "photo",
-    "category": "food",
-    "safesearch": "true"
-}
+article_doi = r'10.1600/036364422X16674053033831'
+article_endpoint = 'http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&groupingFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&FP-doc.doi={}&format=JSON'.format(quote(article_doi, safe=''))
+print(article_endpoint)
 
-response = requests.get(base_url, params=base_params)
-if response.ok:
-  results = response.json()
-  i = 1
+#get the article uuid
+article_response = requests.get(article_endpoint)
+if article_response.ok:
+  results = article_response.json()
+  articles = results['data']
+  if articles and len(articles):
+    article_uuid = articles[0]['DocArticleUuid'] # there should only be one...
+  else:
+    print('article not found...')
 else:
-  print('Oops!', response.content.decode('utf-8'))
+  print('Oops!', article_response.content.decode('utf-8'))
+
+# use uuid here
