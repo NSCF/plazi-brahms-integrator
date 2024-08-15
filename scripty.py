9e9cb1e28ec5991b86cfa8554e5c3fe7f6c6f3ad
Ian Engelbrecht <ianicus.za@gmail.com>
2024-08-15 20:17:59 +0200

    added extra prints

--- scripty.py
+++ scripty.py
@@ -9,7 +9,9 @@ format = '&format=json'
 
 article_doi = r'10.1600/036364422X16674053033831'
 article_endpoint = 'http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&groupingFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&FP-doc.doi={}&format=JSON'.format(quote(article_doi, safe=''))
+print()
 print(article_endpoint)
+print()
 
 #get the article uuid
 article_response = requests.get(article_endpoint)
