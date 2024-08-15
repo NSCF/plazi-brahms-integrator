69ce8f5ffd908809281e610f503d55b1497e42d0
Ian Engelbrecht <ianicus.za@gmail.com>
2024-08-15 20:38:21 +0200

    fixed the darn file name

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ from urllib.parse import quote
 base = 'https://api.plazi.org/v1'
 format = '&format=json'
 
-article_doi = r'10.1600/036364422X16674053033831'
+article_doi = '10.1600/036364422X16674053033831'
 article_endpoint = 'http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&groupingFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&FP-doc.doi={}&format=JSON'.format(quote(article_doi, safe=''))
 print()
 print(article_endpoint)
