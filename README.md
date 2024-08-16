3f10772c461a7b7f6f447ed28d84b0953d896f5b
Ian Engelbrecht <ianicus.za@gmail.com>
2024-08-16 11:21:35 +0200

    updated readme and script

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 ## Plazi BRAHMS8 taxonomy integrator
 
 Initially just a playground for calling data from the Plazi API for integration into BRAHMS8. Will form the basis for something more concrete once things are better understood.
+
+### Installation and usage
+
+You will need [`git`](https://git-scm.com/downloads) and [`Python`](https://www.python.org/downloads/) installed. 
+
+Clone this repo to your local machine, open the folder, click in the address bar of the folder, type cmd and enter to open a command window. 
+
+Run `pip install -r requirements.txt` to install requirements.
+
+Back in the folder, you can open script.py in a text editor like Notepad (not MS Word!) and edit the article_doi variable for the article you want (add just the DOI, not the whole DOI URL). Save your changes.
+
+In the command line run `python script.py`. It will give some output, and that's as far as we are thus far. 
+
+Check back here to see if there are updates (you'll see dates next to the file names), and/or run `git pull` on the command line to get the latest updates, and then `python script.py` again to see what the code is doing. 
+
+I'll be updating the script incrementally as we go along and work towards what we need. 
--- script.py
+++ script.py
@@ -3,12 +3,19 @@
 import requests
 from urllib.parse import quote
 
+
+article_doi = '10.1600/036364422X16674053033831'
+
+
+
+article_endpoint = 'http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&groupingFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&FP-doc.doi={}&format=JSON'.format(quote(article_doi, safe=''))
+
 # for later
 base = 'https://api.plazi.org/v1'
 format = '&format=json'
 
-article_doi = '10.1600/036364422X16674053033831'
-article_endpoint = 'http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&groupingFields=doc.articleUuid+doc.doi+bib.author+bib.title+bib.year+bib.source+bib.volume+bib.issue+bib.numero+bib.firstPage+bib.lastPage+cont.treatCount+cont.matCitCount&FP-doc.doi={}&format=JSON'.format(quote(article_doi, safe=''))
+### SCRIPT ###
+print('Fetching article record at the URL below:')
 print()
 print(article_endpoint)
 print()
@@ -23,6 +30,7 @@ if article_response.ok:
   else:
     print('article not found...')
 else:
-  print('Oops!', article_response.content.decode('utf-8'))
+  print('Oops! Something went wrong...')
+  print(article_response.content.decode('utf-8'))
 
 # use uuid here
